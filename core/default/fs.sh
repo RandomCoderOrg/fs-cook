2e557d4fff0eeb59972c75efd92b5c5db02a307c
Sonyu Annino <92148685+AnninoDr4@users.noreply.github.com>
2021-10-16 16:42:08 +0530

    Update fs.sh

--- core/default/fs.sh
+++ core/default/fs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-source ../../plugins/functions
+source plugins/functions
 
-OUT_DIR="../../out/arm64"
+OUT_DIR="out/arm64"
 
 export ENABLE_EXIT=true
 export OVERRIDER_COMPRESSION_TYPE="gzip"
