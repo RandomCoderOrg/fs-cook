152b026cd4a0251e15e3ef5480e83e34342d7e9c
AnninoDr4 <sonyu.rc@gmail.com>
2021-10-16 10:11:38 +0530

    implement `lz` compression and no exit
    → new lz compression type
    → disable `exit1` ( renable with `ENABLE_EXIT` to `true` )

---
+++ plugins/lz-packer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+target=$1
+target_source=/
+# target_source=
+
+tar \
+    --exclude="${target_source}"/dev/* \
+    --exclude="${target_source}"/run/* \
+    --exclude="${target_source}"/proc/* \
+    --exclude="${target_source}"/sys/* \
+    --exclude="${target_source}"/tmp/* \
+    -cpf \
+     - "${target_source}" -P \
+    | pv -s $(($(du -sk "${target_source}" | awk '{print $1}') * 1024)) |\
+     lz4 -9 > "${target}".tar.gz # use high compression
