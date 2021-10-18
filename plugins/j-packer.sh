da2610cfd9a4883b3a4f716170cae4633a29421d
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-10-18 08:27:02 +0530

    exclude all cahche

--- plugins/j-packer.sh
+++ plugins/j-packer.sh
@@ -10,7 +10,7 @@ tar \
     --exclude="${target_source}"/sys/* \
     --exclude="${target_source}"/tmp/* \
     --exclude="${target}" \
-    --exclude-caches \
+    --exclude-caches-all \
     -cpf \
      - "${target_source}" -P \
     | pv -s $(($(du -sk "${target_source}" | awk '{print $1}') * 1024)) |\
