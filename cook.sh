8b8c70042ccb8f4ca4d2a2c579820c5d5325b143
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-10-31 11:43:44 +0530

    build example

--- cook.sh
+++ cook.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 #shellcheck disable=SC1091
 
+# this is an example file to BUILD raw file system
+# export variable SUITE to set debootstrap suite name (default: hirsute)
+
 source plugins/envsetup
 source plugins/colors
+
+do_debootstrap "out/hirsute-arm64" "arm64"
+do_compress "out/hirsute-arm64"
