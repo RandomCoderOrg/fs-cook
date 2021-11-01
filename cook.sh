f496eabc6fac087e7be220c546210d2932fb12d6
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2021-11-01 09:12:06 +0530

    `impish` base-fs auto builder an example file to build multiple impish filesystems

--- cook.sh
+++ cook.sh
@@ -7,5 +7,16 @@
 source plugins/envsetup
 source plugins/colors
 
-do_debootstrap "out/hirsute-arm64" "arm64"
-do_compress "out/hirsute-arm64"
+#do_debootstrap "out/hirsute-armhf" "armhf"
+#do_compress "out/hirsute-armhf"
+
+export SUITE="impish"
+
+do_debootstrap "out/impish-arm64" "arm64"
+do_compress "out/impish-arm64"
+
+do_debootstrap "out/impish-armhf" "armhf"
+do_compress "out/impish-armhf"
+
+do_debootstrap "out/impish-amd64" "amd64"
+do_compress "out/impish-amd64"
