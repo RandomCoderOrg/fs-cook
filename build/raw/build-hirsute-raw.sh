07b74d5647e41e65bf5b1e9aba86b8788545b59f
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2021-11-17 17:16:26 +0530

    rearrange scripts

---
+++ build/raw/build-hirsute-raw.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#shellcheck disable=SC1091
+
+# this is an example file to BUILD raw file system
+# export variable SUITE to set debootstrap suite name (default: hirsute)
+
+source plugins/envsetup
+source plugins/colors
+
+export OVERRIDER_COMPRESSION_TYPE
+export ENABLE_EXIT
+
+frn="out/hirsute-raw"
+OVERRIDER_COMPRESSION_TYPE="gzip"
+ENABLE_EXIT=true
+
+do_debootstrap "${frn}-arm64" arm64
+do_compress    "${frn}-arm64"
+do_debootstrap "${frn}-armhf" armhf
+do_compress    "${frn}-armhf"
+do_debootstrap "${frn}-amd64" amd64
+do_compress    "${frn}-amd64"
--- core/default/fs.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-source plugins/functions
-
-OUT_DIR="out/arm64"
-
-export ENABLE_EXIT=true
-export OVERRIDER_COMPRESSION_TYPE="gzip"
-
-do_debootstrap $OUT_DIR arm64
