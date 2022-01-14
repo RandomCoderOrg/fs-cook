d65d009ca5dc9ec743ee4755adc59d719a502aff
Zman-1x1 <saicharankandukuri1x1@gmail.com>
2022-01-14 19:00:35 +0530

    add additional_setup() to builds

--- build-impish-raw.sh
+++ build-impish-raw.sh
@@ -19,17 +19,45 @@ ENABLE_EXIT=true
 ENABLE_USER_SETUP=false
 
 additional_setup() {
-    shout "additional_setup"
-    run_cmd echo deb $MIRROR focal main restricted \> /etc/apt/sources.list
-    run_cmd echo deb-src $MIRROR focal main restricted \> /etc/apt/sources.list
-    run_cmd echo deb $MIRROR focal-updates universe \> /etc/apt/sources.list
-    run_cmd echo deb-src $MIRROR focal-updates universe \> /etc/apt/sources.list
-    run_cmd echo deb $MIRROR focal multiverse \> /etc/apt/sources.list
-    run_cmd echo deb-src $MIRROR focal multiverse \> /etc/apt/sources.list
+cat <<-  EOF > $chroot_dir/etc/apt/sources.list
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+deb $MIRROR $SUITE main restricted
+# deb-src $MIRROR $SUITE main restricted
 
-    run_cmd "apt-get update"
+## Major bug fix updates produced after the final release of the
+## distribution.
+deb $MIRROR $SUITE-updates main restricted
+# deb-src $MIRROR $SUITE-updates main restricted
 
-    install_pkg "lz4 bzip2 gzip bc pv"
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team. Also, please note that software in universe WILL NOT receive any
+## review or updates from the Ubuntu security team.
+deb $MIRROR $SUITE universe
+# deb-src $MIRROR $SUITE universe
+deb $MIRROR $SUITE-updates universe
+# deb-src $MIRROR $SUITE-updates universe
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team, and may not be under a free licence. Please satisfy yourself as to
+## your rights to use the software. Also, please note that software in
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb $MIRROR $SUITE multiverse
+# deb-src $MIRROR $SUITE multiverse
+deb $MIRROR $SUITE-updates multiverse
+# deb-src $MIRROR $SUITE-updates multiverse
+
+## N.B. software from this repository may not have been tested as
+## extensively as that contained in the main release, although it includes
+## newer versions of some applications which may provide useful features.
+## Also, please note that software in backports WILL NOT receive any review
+## or updates from the Ubuntu security team.
+deb $MIRROR $SUITE-backports main restricted universe multiverse
+# deb-src $MIRROR $SUITE-backports main restricted universe multiverse
+
+EOF
+
 }
 
 do_build "${frn}-arm64" arm64
--- cook.sh
+++ cook.sh
@@ -18,6 +18,47 @@ OVERRIDER_COMPRESSION_TYPE="gzip"
 ENABLE_EXIT=true
 ENABLE_USER_SETUP=false
 
+additional_setup() {
+cat <<-  EOF > $chroot_dir/etc/apt/sources.list
+# See http://help.ubuntu.com/community/UpgradeNotes for how to upgrade to
+# newer versions of the distribution.
+deb $MIRROR $SUITE main restricted
+# deb-src $MIRROR $SUITE main restricted
+
+## Major bug fix updates produced after the final release of the
+## distribution.
+deb $MIRROR $SUITE-updates main restricted
+# deb-src $MIRROR $SUITE-updates main restricted
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team. Also, please note that software in universe WILL NOT receive any
+## review or updates from the Ubuntu security team.
+deb $MIRROR $SUITE universe
+# deb-src $MIRROR $SUITE universe
+deb $MIRROR $SUITE-updates universe
+# deb-src $MIRROR $SUITE-updates universe
+
+## N.B. software from this repository is ENTIRELY UNSUPPORTED by the Ubuntu
+## team, and may not be under a free licence. Please satisfy yourself as to
+## your rights to use the software. Also, please note that software in
+## multiverse WILL NOT receive any review or updates from the Ubuntu
+## security team.
+deb $MIRROR $SUITE multiverse
+# deb-src $MIRROR $SUITE multiverse
+deb $MIRROR $SUITE-updates multiverse
+# deb-src $MIRROR $SUITE-updates multiverse
+
+## N.B. software from this repository may not have been tested as
+## extensively as that contained in the main release, although it includes
+## newer versions of some applications which may provide useful features.
+## Also, please note that software in backports WILL NOT receive any review
+## or updates from the Ubuntu security team.
+deb $MIRROR $SUITE-backports main restricted universe multiverse
+# deb-src $MIRROR $SUITE-backports main restricted universe multiverse
+
+EOF
+}
+
 do_build "${frn}-arm64" arm64
 do_compress    "${frn}-arm64"
 do_build "${frn}-armhf" armhf
