b08e01d73799b2fe45c9c00ec040dd750beaae91
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2022-12-11 11:02:26 +0530

    add pv fix to all scripts

--- .github/scripts/build-kinetic.sh
+++ .github/scripts/build-kinetic.sh
@@ -57,6 +57,13 @@ deb $MIRROR $SUITE-backports main restricted universe multiverse
 # deb-src $MIRROR $SUITE-backports main restricted universe multiverse
 
 EOF
+
+# set up pv
+lshout "Setting up pv..."
+cp $ROOT_DIR/core/pv/pv-${t_arch}.deb $chroot_dir
+run_cmd "dpkg -i /pv-${t_arch}.deb"
+run_cmd "rm /pv-${t_arch}.deb"
+
 # clean any archive if exits
 apt-get clean
 
