60df6ef9b1e9962217838106403f2e97965f361b
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-12-04 16:09:17 +0530

    feat: add initial works for termux support

--- plugins/lz-packer.sh
+++ plugins/lz-packer.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
 
 target=$1
-target_source=/
-# target_source=
 
 tar \
-    --exclude="${target_source}"dev/* \
-    --exclude="${target_source}"run/* \
-    --exclude="${target_source}"proc/* \
-    --exclude="${target_source}"sys/* \
-    --exclude="${target_source}"tmp/* \
-    --exclude="${target_source}"${0} \
-    --exclude="${target_source}${target}.tar.lz" \
+    --exclude={/dev,/apex,/vendor,/system,/sdcard} \
+    --exclude={/proc/*,/sys/*,/tmp/*,/mnt/*,/media/*,/lost+found/*} \
+    --exclude="*.l2s.*" \
+    --exclude=/${0} \
+    --exclude="/${target}.tar.gz" \
     --exclude-caches-all \
+    --one-file-system \
     -cpf \
-     - "${target_source}" -P \
-    | pv -s $(($(du -skx "${target_source}" | awk '{print $1}') * 1024)) |\
+     - / -P \
+    | pv -s $(($(du -skx / | awk '{print $1}') * 1024)) |\
      lz4 -9 > "${target}".tar.lz # use high compression
