382b647a6fc00d884b29a1f3880dd3e9a4bc45ff
23xvx <noxic34@gmail.com>
2023-12-05 17:39:16 +0800

    Fix errors in proot

--- plugins/j-packer.sh
+++ plugins/j-packer.sh
@@ -3,7 +3,7 @@
 target=$1
 
 tar \
-    --exclude={/dev,/apex,/vendor,/system,/sdcard} \
+    --exclude={/data,/dev,/apex,/vendor,/system,/sdcard} \
     --exclude={/proc/*,/sys/*,/tmp/*,/mnt/*,/media/*,/lost+found/*} \
     --exclude="*.l2s.*" \
     --exclude=/${0} \
@@ -13,4 +13,4 @@ tar \
     -cpf \
      - / -P \
     | pv -s $(($(du -skx / | awk '{print $1}') * 1024)) |\
-     bzip2 --best > "${target}".tar.xz
+     bzip2 --best > /"${target}".tar.xz
