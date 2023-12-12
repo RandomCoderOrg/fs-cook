43a93809b7e7f57e3e202c314eba8adee7cd3976
23xvx <noxic34@gmail.com>
2023-12-12 21:58:29 +0800

    Improve setup-user.sh and packers.sh

--- plugins/setup-user.sh
+++ plugins/setup-user.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash 
+
+# username = FS_USER
+# password = FS_PASS
+
 useradd -m \
+    -p "$(openssl passwd -1 FS_PASS)" \
     -G sudo \
     -d /home/FS_USER \
     -k /etc/skel \
     -s /bin/bash \
     FS_USER
-echo "FS_USER:FS_PASS" | chpasswd
 echo FS_USER ALL=\(root\) ALL > /etc/sudoers.d/FS_USER
 chmod 0440 /etc/sudoers.d/FS_USER
